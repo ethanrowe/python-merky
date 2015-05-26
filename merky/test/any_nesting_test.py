5fb6d20f396bb6e8cca60b2562a85e9e088da004
Ethan Rowe <ethan@the-rowes.com>
2015-05-26 14:08:32 -0400

    Basic nesting transformer
    Provides a walker function and a higher-level Transformer helper
    class for extracting the components of a tree in an iterative
    (non-recursive) manner.
    
    Went for iteration over recursion because of a preference for
    yielding components as they're encountered, which is a lot more
    easily done with iterative implementation than recursive.
    
    This has python 3 compatibility issues to be explored.

--- merky/test/any_nesting_test.py
+++ merky/test/any_nesting_test.py
@@ -1,3 +1,5 @@
+import collections
+
 from nose import tools
 from . import words
 from merky import tree
@@ -18,10 +20,107 @@ BASIC = {"string": "some string",
          "unicode": words.SHEKELS,
         }
 
+BASIC_ORDERED = collections.OrderedDict((
+                    ('false', False),
+                    ('negative integer', -17),
+                    ('positive integer', 25000),
+                    ('string', 'some string'),
+                    ('true', True),
+                    ('unicode', words.SHEKELS),
+                ))
+
+NESTING_DICT = {
+        "another string": "more strings!",
+        "honesty": True,
+        "lies": False,
+        "a": dict(BASIC),
+        "c": dict(BASIC),
+       }
+
+NESTING_DICT_ORDERED = collections.OrderedDict((
+                            ('a', BASIC_HASH),
+                            ('another string', 'more strings!'),
+                            ('c', BASIC_HASH),
+                            ('honesty', True),
+                            ('lies', False),
+                       ))
+
+NESTING_DICT_JSON = words.unify('{',
+                                  '"a":"', BASIC_HASH, '",',
+                                  '"another string":"more strings!"', ',',
+                                  '"c":"', BASIC_HASH, '",'
+                                  '"honesty":true,',
+                                  '"lies":false',
+                                '}')
+
+# hashlib.sha1(NESTING_DICT_JSON.encode("utf-8")).hexdigest()
+NESTING_DICT_HASH = '593a122791ea25479cc6b2c16d69a5fb81241edb'
+
+LIST = ["blah", words.ANGSTROM, words.EUROS]
+LIST_JSON = words.unify('["blah",',
+                        '"', words.ANGSTROM, '",',
+                        '"', words.EUROS, '"',
+                        ']')
+LIST_HASH = '447f4779bf4a9865c600da63256eab9c827e48b0'
+
+NESTING_LIST = ["something",
+                dict(BASIC),
+                list(LIST),
+                words.EPEES]
+
+NESTING_LIST_JSON = words.unify('[',
+                                '"something",'
+                                '"', BASIC_HASH, '",',
+                                '"', LIST_HASH, '",',
+                                '"', words.EPEES, '"',
+                                ']')
+
+NESTING_LIST_HASH = 'd724e888872c0a0545b13c0f16980953df360bf9'
+
+def test_string():
+    t = tree.Transformer()
+    r = t.transform("what?")
+    tools.assert_equal(("74417e40273f06624262871962bd9e738162da47", "what?"), next(r))
+    tools.assert_raises(StopIteration, next, r)
+
+def test_integer():
+    t = tree.Transformer()
+    r = t.transform(54321)
+    tools.assert_equal(("348162101fc6f7e624681b7400b085eeac6df7bd", 54321), next(r))
+    tools.assert_raises(StopIteration, next, r)
 
 def test_basic_dict():
     t = tree.Transformer()
     r = t.transform(BASIC)
-    tools.assert_equal((BASIC_HASH, BASIC), next(r))
+    tools.assert_equal((BASIC_HASH, BASIC_ORDERED), next(r))
+    tools.assert_raises(StopIteration, next, r)
+
+def test_nesting_dict():
+    t = tree.Transformer()
+    r = t.transform(NESTING_DICT)
+    # We know the order because its depth-first traversal.
+    # We also expect the hash to be the same for each copy of BASIC.
+
+    # For the "a" member.
+    tools.assert_equal((BASIC_HASH, BASIC_ORDERED), next(r))
+    # For the "c" member.
+    tools.assert_equal((BASIC_HASH, BASIC_ORDERED), next(r))
+    # For the top container.  Note substituion of hashes.
+    tools.assert_equal((NESTING_DICT_HASH, dict(NESTING_DICT, a=BASIC_HASH, c=BASIC_HASH)), next(r))
+    tools.assert_raises(StopIteration, next, r)
+
+def test_basic_list():
+    t = tree.Transformer()
+    r = t.transform(LIST)
+    tools.assert_equal((LIST_HASH, LIST), next(r))
+    tools.assert_raises(StopIteration, next, r)
+
+def test_nesting_list():
+    t = tree.Transformer()
+    r = t.transform(NESTING_LIST)
+
+    tools.assert_equal((BASIC_HASH, BASIC_ORDERED), next(r))
+    tools.assert_equal((LIST_HASH, LIST), next(r))
+    tools.assert_equal((NESTING_LIST_HASH, ["something", BASIC_HASH, LIST_HASH, words.EPEES]), next(r))
     tools.assert_raises(StopIteration, next, r)
 
--- merky/tree.py
+++ merky/tree.py
@@ -1,20 +1,127 @@
+import collections
+import functools
 import hashlib
+import itertools
 import json
 
 def hexdigest(encodable):
     return hashlib.sha1(encodable.encode("utf-8")).hexdigest()
 
 
+def hexdigester(serializer):
+    def inner(structure):
+        return hexdigest(serializer(structure))
+    return inner
+
+
+def string_handler(item):
+    if hasattr(item, "encode"):
+        return item, None, False
+    raise TypeError
+
+
+def flatten(sequence_of_sequences):
+    return itertools.chain.from_iterable(sequence_of_sequences)
+
+def pairwise(sequence):
+    sequence = iter(sequence)
+    try:
+        while True:
+            yield next(sequence), next(sequence)
+    except StopIteration:
+        pass
+
+
+def ordered_map(sequence):
+    return collections.OrderedDict(sorted(pairwise(sequence)))
+
+
+def map_handler(item):
+    try:
+        i = flatten(sorted(item.iteritems()))
+        return i, ordered_map, True
+    except AttributeError:
+        raise TypeError
+
+
+def seq_handler(item):
+    return iter(item), list, True
+
+
+def default_handler(item):
+    return item, None, False
+
+
+def dispatcher(*handlers):
+    def inner(item):
+        for handler in handlers:
+            try:
+                return handler(item)
+            except TypeError:
+                pass
+        raise TypeError("No handler could be found for item of type %s" % type(item))
+    return inner
+
+full_nesting_dispatcher = dispatcher(string_handler,
+                                     map_handler,
+                                     seq_handler,
+                                     default_handler)
+
+
+def walker(structure, dispatcher, tokenizer):
+    stack = []
+    accum = []
+    current, collector, tokenize = iter((structure,)), lambda x: x, False
+    while True:
+        try:
+            next_item, next_col, next_tok = dispatcher(next(current))
+            if next_col:
+                stack.append((current, collector, tokenize, accum))
+                current, collector, tokenize, accum = next_item, next_col, next_tok, []
+            else:
+                accum.append(next_item)
+        except StopIteration:
+            value = collector(accum)
+            if tokenize:
+                t = tokenizer(value)
+                yield (t, value)
+                value = t
+
+            if not stack:
+                break
+
+            current, collector, tokenize, accum = stack.pop()
+            accum.append(value)
+
+
 class Transformer(object):
     def __init__(self):
-        self.encoder = self.get_encoder()
+        self.serializer = self.get_serializer()
+        self.tokenizer = self.get_tokenizer()
+        self.dispatcher = self.get_dispatcher()
+
+    def walker(self, structure):
+        return walker(structure, full_nesting_dispatcher, self.tokenizer)
+
 
     def transform(self, structure):
-        yield hexdigest(self.encoder.encode(structure)), structure
+        seen = False
+        for pair in self.walker(structure):
+            yield pair
+            seen = True
+        if not seen:
+            yield (self.tokenizer(structure), structure)
 
-    def get_encoder(self):
+    def get_serializer(self):
         return json.JSONEncoder(ensure_ascii=False,
                                 allow_nan=False,
                                 sort_keys=True,
-                                separators=(",",":"))
+                                separators=(",",":")).encode
+
+    def get_dispatcher(self):
+        return full_nesting_dispatcher
+
+
+    def get_tokenizer(self):
+        return hexdigester(self.serializer)
 
