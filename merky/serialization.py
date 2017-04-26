51f82605b521bb80a3bb6deb8675e577d43d0d8d
Ethan Rowe <ethanrowe000@gmail.com>
2017-04-26 15:52:01 -0400

    JSON encoded handles annotated objects
    Alters the JSON serialization function builder so the resulting encoder
    will be able to handle annotated objects by simply handling the
    underlying wrapped object.

--- merky/serialization.py
+++ merky/serialization.py
@@ -1,14 +1,25 @@
 import json
 
+def _json_default(item):
+    try:
+        return item.__merky_annotated__
+    except:
+        raise TypeError(
+                "object of type %s is not JSON-serializable." % str(type(item)))
+
 def json_serializer(sort=True):
     """
     Returns a function that can serialize to JSON, with unicode enabled,
     nans disallowed, keys sorted, and whitespace-free separators.
+
+    The function will handle merky-annotated objects according to the particulars
+    of the underlying annotated structure.
 
     You can override the sorted keys via `sort=False`.
     """
     return json.JSONEncoder(ensure_ascii=False,
                             allow_nan=False,
                             sort_keys=sort,
-                            separators=(",",":")).encode
+                            separators=(",",":"),
+                            default=_json_default).encode
 
